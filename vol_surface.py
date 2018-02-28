8151beb69deb7bba9685b01b05861e0f5c95f628
Ubuntu <ubuntu@ip-172-31-17-71.us-west-2.compute.internal>
2018-02-28 04:30:24 +0000

    matplotlib headless

--- vol_surface.py
+++ vol_surface.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
